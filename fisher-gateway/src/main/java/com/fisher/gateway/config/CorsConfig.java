a8de4066becf8b957ea8555c2d9841caf945e926
sunhaibj <sunhaiseec@gmail.com>
2019-02-28 17:28:16 +0800

    fixed:跨域问题

---
+++ fisher-gateway/src/main/java/com/fisher/gateway/config/CorsConfig.java
@@ -0,0 +1,35 @@
+package com.fisher.gateway.config;
+
+/**
+ * @author sunhaibj
+ * @date 2019/2/28
+ */
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+/**
+ * description:
+ *
+ * @author sunhai
+ * @date 2019/01/02
+ */
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
