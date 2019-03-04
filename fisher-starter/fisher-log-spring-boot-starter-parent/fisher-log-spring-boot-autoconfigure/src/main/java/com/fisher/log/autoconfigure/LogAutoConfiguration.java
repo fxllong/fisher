29075f0f10b28f5de6385dcd066e446aebb605a0
fanxinglong <fxllong262553@gmail.com>
2019-03-04 16:00:45 +0800

    日志自动配置修改

--- fisher-starter/fisher-log-spring-boot-starter-parent/fisher-log-spring-boot-autoconfigure/src/main/java/com/fisher/log/autoconfigure/LogAutoConfiguration.java
+++ fisher-starter/fisher-log-spring-boot-starter-parent/fisher-log-spring-boot-autoconfigure/src/main/java/com/fisher/log/autoconfigure/LogAutoConfiguration.java
@@ -3,13 +3,18 @@ package com.fisher.log.autoconfigure;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.core.Queue;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConditionalOnClass(SysLogAspect.class)
 public class LogAutoConfiguration {
 
+
     @Bean
+    @ConditionalOnMissingBean(SysLogAspect.class)
     public SysLogAspect sysLogAspect() {
         return new SysLogAspect();
     }
