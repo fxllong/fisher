eb64d88168578a1679dcbaf2674b76a680c5ffe5
fanxinglong <fxllong262553@gmail.com>
2019-03-12 18:28:33 +0800

    bug fix

--- db/fisher_gen.sql
+++ db/fisher_gen.sql
@@ -12,4 +12,4 @@ CREATE TABLE `sys_gen_db_config` (
                                    `user_name` varchar(255) NOT NULL COMMENT '用户名',
                                    `password` varchar(255) NOT NULL COMMENT '密码',
                                    PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='代码生成器配置表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='代码生成表';
